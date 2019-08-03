f26a1e70c03a96d27f3c6e41577b996b865fe217
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-08-03 19:58:42 -0400

    Implement model

--- server/db.js
+++ server/db.js
@@ -1,1 +1,12 @@
-const mongo = require('mongodb');
+const mongo = require('mongodb').MongoClient;
+const host = process.env.HOST || 'localhost';
+const port = process.env.DB_PORT || '27017';
+
+const performOperation = operation => (
+  mongo.connect(`mongodb://${host}/${port}`)
+    .then(client => client.db('team-builder'))
+    .then(db => db.collection('groups'))
+    .then(operation)
+);
+
+module.exports = performOperation;
---
+++ server/model.js
@@ -0,0 +1,21 @@
+const { ObjectID } = require('mongodb');
+const performOperation = require('./db.js');
+
+module.exports.add = group => (
+  performOperation(collection => collection.insertOne(group))
+);
+
+module.exports.find = (groupId) => {
+  if (groupId) {
+    return performOperation(collection => (
+      collection.findOne({ _id: ObjectID(groupId) })
+    ));
+  }
+  return performOperation(collection => collection.find().toArray());
+};
+
+module.exports.delete = groupId => (
+  performOperation(collection => (
+    collection.deleteOne({ _id: ObjectID(groupId) })
+  ))
+);
