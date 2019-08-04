1cb13f964ba6b47e7e3b29085ad33e1419636053
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-08-03 20:03:17 -0400

    Implement GET route

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const path = require('path');
+const groups = require('./model.js');
 const port = process.env.PORT || 2000;
 const app = express();
 
 app.use(express.static('dist'));
+
+app.get('/groups/:id?', (req, res) => {
+  const { id } = req.params;
+  groups.find(id)
+    .then(group => res.json(group))
+    .catch(err => res.json(err));
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
