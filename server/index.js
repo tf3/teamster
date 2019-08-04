dfcad156d949122d1e1b2ff92a5b6e504701b348
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-08-03 20:08:53 -0400

    Implement POST route

--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
 const express = require('express');
-const path = require('path');
 const groups = require('./model.js');
 const port = process.env.PORT || 2000;
 const app = express();
 
 app.use(express.static('dist'));
+app.use(express.json());
 
 app.get('/groups/:id?', (req, res) => {
   const { id } = req.params;
   groups.find(id)
     .then(group => res.json(group))
+    .catch(err => res.json(err));
+});
+
+app.post('/groups', (req, res) => {
+  const group  = req.body;
+  groups.add(group)
+    .then(() => res.json(group))
     .catch(err => res.json(err));
 });
 
