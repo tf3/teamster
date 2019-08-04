206786c308319426e0431bbe9afa0d8d103241e8
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-08-04 18:58:36 -0400

    Change database configuration

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const host = process.env.HOST || 'localhost';
 const port = process.env.DB_PORT || '27017';
 
 const performOperation = operation => (
-  mongo.connect(`mongodb://${host}/${port}`)
+  mongo.connect(`mongodb://${host}/${port}`, { useNewUrlParser: true })
     .then(client => client.db('team-builder'))
     .then(db => db.collection('groups'))
     .then(operation)
