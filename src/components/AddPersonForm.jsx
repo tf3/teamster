32831566b5198436e98c1f5bc15435d3accf0a49
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-08-06 18:17:53 -0400

    Add AddPersonForm component

---
+++ src/components/AddPersonForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AddPersonForm = () => (
+  <form>
+    <label>
+      New person
+      <input type="text" />
+    </label>
+  </form>
+);
+
+export default AddPersonForm;
--- src/components/UnassignedList.jsx
+++ src/components/UnassignedList.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import AddPersonForm from './AddPersonForm.jsx';
 
 const UnassignedList = ({ unassigned }) => (
   <div className="unassigned">
     <ol>
       {unassigned.map(person => <li>{person.name}</li>)}
     </ol>
+    <AddPersonForm />
   </div>
 );
 
