77e352efedbb673f5223a92c280cdd5ae41660ed
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-08-07 18:44:01 -0400

    Show display message if team has no members

--- src/components/TeamList.jsx
+++ src/components/TeamList.jsx
@@ -3,9 +3,13 @@ import React from 'react';
 const TeamList = ({ team }) => (
   <div className="team">
     <h3>{team.name} ({team.members.length}/{team.maxMembers})</h3>
-    <ol>
-      {team.members.map(member => <li>{member.name}</li>)}
-    </ol>
+    {team.members.length === 0 ? (
+      <p>No members</p>
+    ) : (
+      <ol>
+        {team.members.map(member => <li>{member.name}</li>)}
+      </ol>
+    )}
   </div>
 );
 
